0bc280c51499d8c955e7159bc08ac8666a4fcb1d
MJahnavi23 <ammu.mallipeddi@gmail.com>
2024-03-17 19:40:38 +0530

    convert react to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React, {useState} from 'react';
 import './App.css';
-import TodoTable from './components/TodoTable'
-import NewTodoForm from './components/NewTodoForm';
+import { TodoTable } from './components/TodoTable'
+import { NewTodoForm } from './components/NewTodoForm';
 
-function App() {
+export const App = () => {
 
   const [showAddTodoForm, setShowAddTodoForm] = useState(false);
 
@@ -14,7 +14,7 @@ function App() {
     // {rowNumber: 4, rowDescription: "Charge Phone", rowAssigned: "Pop"},
   ])
 
-  const addTodo = (description, assigned) => {
+  const addTodo = (description: string, assigned: string) => {
     let rowNumber = 0;
     if(todos !== null || todos !== undefined){
       rowNumber = todos[todos.length - 1].rowNumber + 1;
@@ -30,7 +30,7 @@ function App() {
       // console.log(todos);
   }
 
-  const deleteTodo = (deleteTodoRowNumber) => {
+  const deleteTodo = (deleteTodoRowNumber: number) => {
     let filtered = todos.filter(function (value){
       return value.rowNumber !== deleteTodoRowNumber;
     });
@@ -56,5 +56,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
